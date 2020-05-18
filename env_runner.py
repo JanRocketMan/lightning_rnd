738b28793f376f67d86ffe2be00c33dd8a451505
JanRocketMan <cherv277@gmail.com>
2020-05-18 23:02:17 +0300

    Added two pipes for env_runner and trainer, now need to configure launch

--- env_runner.py
+++ env_runner.py
@@ -1,9 +1,15 @@
+from copy import deepcopy
+import traceback
+from threading import Lock
+
 import numpy as np
-from torch.multiprocessing import Pipe
+from torch.multiprocessing import Pipe, Process
+import torch
 from config import default_config
 
+from rnd_agent import RNDPPOAgent
 from environments import AtariEnvironmentWrapper
-from utils import RunningMeanStd, RewardForwardFilter
+from utils import RunningMeanStd
 
 
 ENV_NAME = default_config["EnvName"]
@@ -14,11 +20,19 @@ IMAGE_WIDTH = default_config["ImageWidth"]
 INIT_STEPS = default_config["NumInitSteps"]
 
 
-class ParallelEnvironmentRunner:
-    def __init__(self, num_workers, action_dim, render_envs=False):
+class ParallelEnvironmentRunner(Process):
+    def __init__(
+        self, num_workers: int, action_dim: int, rollout_steps: int,
+        init_agent: RNDPPOAgent, init_state: np.array,
+        conn_to_learner, writer, render_envs=False
+    ):
         self.num_workers = num_workers
         self.render_envs = render_envs
         self.action_dim = action_dim
+        self.rollout_steps = rollout_steps
+        self.writer = writer
+        self.conn_to_learner = conn_to_learner
+        self.actor_agent = deepcopy(init_agent)
         self.all_works = []
         self.parent_conns = []
         self.child_conns = []
@@ -27,65 +41,150 @@ class ParallelEnvironmentRunner:
         self.observation_stats = RunningMeanStd(
             shape=(1, 1, IMAGE_HEIGHT, IMAGE_WIDTH)
         )
+        self.init_state = init_state
+        self.reset_current_state()
 
         self.__init_workers()
         self.__init_obs_stats()
 
+        self.log_env = 0
+        self.log_episode, self.log_steps = 0, 0
+        self.log_reward, self.log_total_steps = 0.0, 0
+
+    def reset_current_state(self):
+        self.current_state = np.zeros(
+            (self.num_workers,) + self.init_state.shape,
+            dtype=np.uint8
+        )
+        for w in range(self.num_workers):
+            self.current_state[w] = self.init_state
+
     def reset_stored_data(self):
+        W, T = self.num_workers, self.rollout_steps
         self.stored_data = {
-            'next_states': np.zeros(
-                (self.num_workers, 4, IMAGE_HEIGHT, IMAGE_WIDTH),
-                dtype=np.float32
+            'states': np.zeros(
+                (W, T, 4, IMAGE_HEIGHT, IMAGE_WIDTH),
+                dtype=np.uint8
             ),
-            'rewards': np.zeros(self.num_workers, dtype=np.float32),
-            'dones': np.zeros(self.num_workers, dtype=np.bool),
-            'real_dones': np.zeros(self.num_workers, dtype=np.bool)
+            'next_states': np.zeros(
+                (W, T, 4, IMAGE_HEIGHT, IMAGE_WIDTH),
+                dtype=np.uint8
+            ),
+            'actions': np.zeros((W, T), dtype=np.uint8),
+            'rewards': np.zeros((W, T), dtype=np.float32),
+            'dones': np.zeros((W, T), dtype=np.bool),
+            'real_dones': np.zeros((W, T), dtype=np.bool),
+            'ext_values': np.zeros((W, T + 1), dtype=np.float32),
+            'int_values': np.zeros((W, T + 1), dtype=np.float32),
+            'policies': np.zeros((W, T, self.action_dim), dtype=np.float32),
+            'log_prob_policies': np.zeros((W, T), dtype=np.float32),
+            'obs_stats': np.zeros((2, IMAGE_HEIGHT, IMAGE_WIDTH), dtype=np.float32)
         }
 
-    def preprocess_obs(self, some_states):
-        return np.clip(
-            (some_states[:, 3, :, :].reshape(-1, 1, IMAGE_HEIGHT, IMAGE_WIDTH) - self.observation_stats.mean) /\
-                self.observation_stats.std, -5, 5
-        )
-
-    def collect_results(self, actions):
+    def collect_env_results(self, actions, step_idx):
+        """Synchronizes environments after each step"""
         for parent_conn, action in zip(self.parent_conns, actions):
             parent_conn.send(action)
 
         for j, parent_conn in enumerate(self.parent_conns):
             new_state, reward, done, real_done = parent_conn.recv()
 
-            self.stored_data['next_states'][j] = new_state
-            self.stored_data['rewards'][j] = reward
-            self.stored_data['dones'][j] = done
-            self.stored_data['real_dones'][j] = real_done
+            self.stored_data['next_states'][j, step_idx] = new_state
+            self.stored_data['rewards'][j, step_idx] = reward
+            self.stored_data['dones'][j, step_idx] = done
+            self.stored_data['real_dones'][j, step_idx] = real_done
 
-    def run_agent(self, agent, states, compute_int_reward=False, update_stats=True):
-        self.reset_stored_data()
+    def run_agent_step(self, step_idx, action_fn, compute_int_reward=False, update_stats=True):
+        # Predict next actions via current agent
+        with torch.no_grad():
+            action, ext_value, int_value, policy = action_fn(
+                self.current_state.astype('float') / 255
+            )
+            log_prob_policy = self.actor_agent.get_policy_log_prob(
+                action, policy
+            )
 
-        actions, ext_value, int_value, policy = agent.get_action(
-            states / 255
-        )
+        # Run and collect results across environments
+        self.collect_env_results(action, step_idx)
+        # Collect now model-based data
+        self.stored_data['actions'][:, step_idx] = action
+        self.stored_data['states'][:, step_idx] = self.current_state
+        self.stored_data['ext_values'][:, step_idx] = ext_value
+        self.stored_data['int_values'][:, step_idx] = int_value
+        self.stored_data['policies'][:, step_idx] = policy
+        self.stored_data['log_prob_policies'][:, step_idx] = log_prob_policy
 
-        self.collect_results(actions)
-
+        # Update observation stats
         if update_stats:
             self.observation_stats.update(
-                self.stored_data['next_states'][:, 3, :, :].reshape(-1, 1, IMAGE_HEIGHT, IMAGE_WIDTH)
+                self.stored_data['next_states'][:, step_idx, 3, :, :].reshape(-1, 1, IMAGE_HEIGHT, IMAGE_WIDTH)
             )
-
-        ret_dict = self.stored_data
-        ret_dict["actions"] = actions
-        ret_dict["ext_value"] = ext_value
-        ret_dict["int_value"] = int_value
-        ret_dict["policy"] = policy
+            self.stored_data['obs_stats'][0] = self.observation_stats.mean.reshape(IMAGE_HEIGHT, IMAGE_WIDTH)
+            self.stored_data['obs_stats'][1] = self.observation_stats.std.reshape(IMAGE_HEIGHT, IMAGE_WIDTH)
 
         if compute_int_reward:
-            ret_dict['intrinsic_reward'] = agent.get_intrinsic_reward(
-                self.preprocess_obs(self.stored_data['next_states'])
+            self.stored_data['int_rewards'][:, step_idx] = self.actor_agent.get_intrinsic_reward(
+                self.preprocess_obs(self.stored_data['next_states'][:, step_idx])
             )
 
-        return ret_dict
+    def run_agent(self, buffer, agent_state, num_epochs):
+        try:
+            super(ParallelEnvironmentRunner, self).run()
+            for _ in range(num_epochs):
+                finished = self.conn_to_learner.recv()
+
+                self.log_total_steps += 1
+                self.reset_stored_data()
+
+                with Lock():
+                    self.actor_agent.load_state_dict(agent_state)
+
+                for idx in range(self.rollout_steps):
+                    self.run_agent_step(
+                        idx, self.actor_agent.get_action
+                    )
+                    self.current_state = deepcopy(self.stored_data["next_states"][:, idx])
+
+                    self.log_current_results(idx)
+
+                with torch.no_grad():
+                    _, ext_value, int_value, _ = self.actor_agent.get_action(
+                        self.current_state.astype('float') / 255
+                    )
+                    self.stored_data['ext_values'][:, self.rollout_steps] = ext_value
+                    self.stored_data['int_values'][:, self.rollout_steps] = int_value
+
+                with Lock():
+                    for key in self.stored_data.keys():
+                        buffer[key] = deepcopy(self.stored_data[key])
+
+                self.conn_to_learner.send(
+                    True
+                )
+
+        except KeyboardInterrupt:
+            pass  # Return silently.
+        except Exception as e:
+            print("Exception in actor process")
+            traceback.print_exc()
+            print()
+            raise e
+
+    def log_current_results(self, step_idx):
+        self.log_reward += self.stored_data['rewards'][self.log_env, step_idx]
+        self.log_steps += 1
+        if self.stored_data['real_dones'][self.log_env]:
+            self.log_episode += 1
+            self.writer.add_scalar('data/reward_per_rollout', self.log_reward, self.log_total_steps)
+            self.writer.add_scalar('data/reward_per_episode', self.log_reward, self.log_episode)
+            self.writer.add_scalar('data/steps_per_episode', self.log_steps, self.log_episode)
+            self.writer.add_scalar(
+                'data/max_prob_per_episode', torch.softmax(
+                    torch.from_numpy(self.stored_data["policies"][:, step_idx]),
+                    -1
+                ).max(1)[0].mean().item()
+            )
+            self.log_reward, self.log_steps = 0.0, 0
 
     def __init_workers(self):
         for i in range(self.num_workers):
@@ -101,21 +200,24 @@ class ParallelEnvironmentRunner:
             self.child_conns.append(child_conn)
 
     def __init_obs_stats(self):
-        class DummyAgent:
-            def __init__(self, num_workers, action_dim):
-                self.num_workers = num_workers
-                self.action_dim = action_dim
-            def get_action(self, state):
-                return np.random.randint(
-                    0, self.action_dim, size=(self.num_workers)
-                ), 0.0, 0.0, 0.0
+        rand_act = lambda x: np.random.randint(
+            0, self.action_dim, size=(self.num_workers)
+        ), 0.0, 0.0, 0.0
+
         self.reset_stored_data()
-        for _ in range(INIT_STEPS):
-            _ = self.run_agent(
-                DummyAgent(self.num_workers, self.action_dim),
-                self.stored_data['next_states']
+        for idx in range(INIT_STEPS):
+            self.run_agent_step(
+                idx,
+                rand_act
             )
         self.reset_stored_data()
+        self.reset_current_state()
+
+    def preprocess_obs(self, some_states):
+        return np.clip(
+            (some_states[:, 3, :, :].reshape(-1, 1, IMAGE_HEIGHT, IMAGE_WIDTH) - self.observation_stats.mean) /\
+                self.observation_stats.std, -5, 5
+        )
 
     def join_all_workers(self):
         for work in self.all_works:
--- trainer.py
+++ trainer.py
@@ -1,6 +1,9 @@
 import numpy as np
 import torch
 from copy import deepcopy
+import traceback
+from threading import Lock
+
 from torch.optim import Adam
 from tensorboardX import SummaryWriter
 
@@ -12,10 +15,9 @@ from utils import RewardForwardFilter, RunningMeanStd, make_train_data
 from torch import multiprocessing as mp
 from torch.multiprocessing import Process, Pipe, set_start_method
 try:
-     set_start_method('spawn')
+    set_start_method('spawn')
 except RuntimeError:
     pass
-
 
 import time
 
@@ -34,174 +36,109 @@ IMAGE_HEIGHT = default_config["ImageHeight"]
 IMAGE_WIDTH = default_config["ImageWidth"]
 
 
-class ActorRolloutRunner(Process):
-    def __init__(
-            self,
-            child_conn,
-            env_runner,
-            init_agent,
-            agent_device='cuda:1'
-        ):
-        self.child_conn = child_conn
-        self.device = agent_device
-        self.env_runner = env_runner
-        self.actor_agent = deepcopy(init_agent).to(agent_device)
-
-        self.reward_stats = RunningMeanStd()
-        self.disc_reward = RewardForwardFilter(INT_REWARD_DISCOUNT)
-
-        self.logged_worker_id = 0
-        self.log_reward = 0.0
-        self.log_steps = 0
-        self.log_episode = 0
-
-        self.current_states = self.env_runner.stored_data["next_states"]
-        super(ActorRolloutRunner, self).__init__()
-
-    def reset_rollout_data(self):
-        self.stored_data = {
-            key: [] for key in [
-                'states', 'next_states', 'rewards', 'dones', 'real_dones',
-                'actions', 'ext_value', 'int_value', 'policy',
-                'intrinsic_reward'
-            ]
-        }
-        self.logger_hist = []
-
-    def normalize_rewards(self):
-        rewards_per_worker = np.array([
-            self.disc_reward.update(reward_per_step) for
-            reward_per_step in self.stored_data["intrinsic_reward"].T
-        ]).reshape(-1)
-        self.reward_stats.update(rewards_per_worker)
-        self.stored_data["intrinsic_reward"] /= (self.reward_stats.std + 1e-6)
-        self.stored_data["rewards"] = np.clip(self.stored_data["rewards"], -1, 1)
-
-    def accumulate_rollout_data(self):
-        self.reset_rollout_data()
-        with torch.no_grad():
-            for step in range(ROLLOUT_STEPS + 1):
-                # Play next step
-                result = self.env_runner.run_agent(
-                    self.actor_agent, self.current_states, compute_int_reward=True
-                )
-                # Append new data to existing and update states
-                for key in ['ext_value', 'int_value']:
-                    self.stored_data[key].append(result[key])
-                if step < ROLLOUT_STEPS:
-                    self.log_reward += result["rewards"][self.logged_worker_id]
-                    self.log_steps += 1
-                    for key in result.keys() - ['ext_value', 'int_value', 'states']:
-                        self.stored_data[key].append(result[key])
-                    self.stored_data['states'].append(self.current_states)
-                    self.current_states = result['next_states']
-
-                # Log stats
-                if result['real_dones'][self.logged_worker_id]:
-                    self.log_episode += 1
-                    self.logger_hist.append(
-                        [self.log_reward, self.log_episode, self.log_steps]
-                    )
-                    self.log_reward, self.log_steps = 0.0, 0
-
-        # Transpose data s.t. first dim corresponds to num_workers
-        for key in self.stored_data.keys():
-            self.stored_data[key] = np.swapaxes(np.stack(
-                self.stored_data[key]), 0, 1
-            )
-        # Normalize states and rewards
-        self.stored_data["states"] /= 255.0
-        self.normalize_rewards()
-
-    def run(self):
-        super(ActorRolloutRunner, self).run()
-        while True:
-            current_agent = self.child_conn.recv()
-            self.actor_agent = deepcopy(current_agent).to(self.device)
-            self.accumulate_rollout_data()
-            del current_agent
-            self.child_conn.send(
-                (self.stored_data, self.env_runner.observation_stats, self.logger_hist)
-            )
-
-
 def preprocess_obs(some_states, observation_stats):
+    obs_mean, obs_std = observation_stats[0], observation_stats[1]
     return np.clip(
-        (some_states[:, 3, :, :].reshape(-1, 1, IMAGE_HEIGHT, IMAGE_WIDTH) - observation_stats.mean) /\
-            observation_stats.std, -5, 5
+        (some_states[:, 3, :, :].reshape(-1, 1, IMAGE_HEIGHT, IMAGE_WIDTH) - obs_mean) /\
+            obs_std, -5, 5
     )
 
 
-class RNDTrainer:
-    def __init__(self, env_runner: ParallelEnvironmentRunner, agent: RNDPPOAgent,
-        logger: SummaryWriter, opt_device: str, run_device: str
+class RNDTrainer(Process):
+    def __init__(self, num_workers, loader_num_workers, conn_to_actor, agent: RNDPPOAgent,
+        logger: SummaryWriter
     ):
-        self.env_runner = env_runner
-        self.num_workers = self.env_runner.num_workers
-        self.device = opt_device
+        self.num_workers = num_workers
+        self.loader_num_workers = loader_num_workers
+        self.conn_to_actor = conn_to_actor
 
-        self.agent = agent.to(self.device)
+        self.agent = deepcopy(agent)
         self.agent_optimizer = Adam(
             [p for p in self.agent.parameters() if p.requires_grad], lr=LEARNING_RATE
         )
 
         self.logger = logger
-
-        self.n_steps = 0
         self.n_updates = 0
-        self.log_episode, self.log_episode_offset = 0, 0
+        self.n_steps = 0
 
-        parent_conn, child_conn = Pipe()
+        self.reward_stats = RunningMeanStd()
+        self.disc_reward = RewardForwardFilter(INT_REWARD_DISCOUNT)
 
-        self.rollout_parent = parent_conn
-        self.rollout_child = child_conn
-        self.rollout_runner = ActorRolloutRunner(
-            self.rollout_child, env_runner,
-            self.agent, run_device
+        self.conn_to_actor.start()
+
+    def get_intrinsic_rewards(self):
+        curr_data = self.stored_data["next_steps"].reshape(-1, 4, IMAGE_HEIGHT, IMAGE_WIDTH)
+        next_rewards = self.agent.get_intrinsic_reward(
+            preprocess_obs(curr_data.astype('float'), self.stored_data["obs_stats"])
         )
-        self.rollout_runner.start()
+        self.stored_data["intrinsic_rewards"] = next_rewards.reshape(self.stored_data["rewards"].shape)
 
-    def train(self, num_epochs, state_dict=None):
+    def normalize_rewards(self):
+        rewards_per_worker = np.array([
+            self.disc_reward.update(reward_per_step) for
+            reward_per_step in self.stored_data["intrinsic_rewards"].T
+        ]).reshape(-1)
+        self.reward_stats.update(rewards_per_worker)
+        self.stored_data["intrinsic_rewards"] /= (self.reward_stats.std + 1e-6)
+        self.stored_data["rewards"] = np.clip(self.stored_data["rewards"], -1, 1)
+
+    def train(self, buffer, agent_state, num_epochs, state_dict=None):
         if state_dict is not None:
             self.load_state_dict(state_dict)
+        self.conn_to_actor.send(True)
 
-        self.rollout_parent.send(self.agent) # SEND START
-        for k in range(num_epochs):
-            self.n_steps += (self.env_runner.num_workers * ROLLOUT_STEPS)
-            self.n_updates += 1
-            with torch.no_grad():
-                result = self.rollout_parent.recv()
-                self.stored_data = deepcopy(result[0])
-                obs_stats, logger_hist = deepcopy(result[1]), deepcopy(result[2])
-                del result
-                self.rollout_parent.send(self.agent)
+            for k in range(num_epochs):
+                finished = self.conn_to_actor.recv()
 
-                self.log_rollout_step_results(logger_hist)
+                self.n_updates += 1
+                self.n_steps += (self.num_workers * ROLLOUT_STEPS)
 
-                ext_target, ext_adv = make_train_data(
-                    self.stored_data["rewards"], self.stored_data["dones"],
-                    self.stored_data["ext_value"], EXT_DISCOUNT,
-                    ROLLOUT_STEPS, self.num_workers
-                )
-                int_target, int_adv = make_train_data(
-                    self.stored_data["intrinsic_reward"], np.zeros_like(
-                        self.stored_data["intrinsic_reward"]
-                    ),
-                    self.stored_data["int_value"], INT_DISCOUNT,
-                    ROLLOUT_STEPS, self.num_workers
-                )
+                with Lock():
+                    self.stored_data = deepcopy(buffer)
+
+                with torch.no_grad():
+                    self.get_intrinsic_rewards()
+                    self.normalize_rewards()
+
+                    ext_target, ext_adv = make_train_data(
+                        self.stored_data["rewards"], self.stored_data["dones"],
+                        self.stored_data["ext_values"], EXT_DISCOUNT,
+                        ROLLOUT_STEPS, self.num_workers
+                    )
+                    int_target, int_adv = make_train_data(
+                        self.stored_data["intrinsic_rewards"], np.zeros_like(
+                            self.stored_data["intrinsic_rewards"]
+                        ),
+                        self.stored_data["int_values"], INT_DISCOUNT,
+                        ROLLOUT_STEPS, self.num_workers
+                    )
+
                 total_adv = INT_COEFF * int_adv + EXT_COEFF * ext_adv
                 c_loader = self.pack_to_dataloader(
                     ext_target, int_target, total_adv, 
                     preprocess_obs(
                         self.stored_data["next_states"].reshape(-1, 4, 84, 84),
-                        obs_stats
+                        self.stored_data["obs_stats"]
                     )
                 )
-            self.train_step(c_loader)
 
-            if self.n_updates % 100 == 0:
-                torch.save(self.state_dict(), SAVE_PATH)
+                self.train_step(c_loader)
+
+                with Lock():
+                    agent_state = deepcopy(self.agent.state_dict())
+
+                self.conn_to_actor.send(True)
+
+                if self.n_updates % 100 == 0:
+                    torch.save(self.state_dict(), SAVE_PATH)
+
+        except KeyboardInterrupt:
+            pass  # Return silently.
+        except Exception as e:
+            print("Exception in actor process")
+            traceback.print_exc()
+            print()
+            raise e
 
     def pack_to_dataloader(self, ext_target, int_target, total_adv, next_states):
         from torch.utils import data
@@ -214,25 +151,20 @@ class RNDTrainer:
             ]
         ]
         next_states_tensor = torch.FloatTensor(next_states)
-        policies_tensor = torch.FloatTensor(
-            self.stored_data["policy"].reshape(-1, self.stored_data["policy"].shape[-1])
+        log_prob_old = torch.FloatTensor(
+            self.stored_data["log_prob_policies"].reshape(-1)
         )
-
-        with torch.no_grad():
-            log_prob_old = self.agent.get_policy_log_prob(
-                actions_tensor, policies_tensor
-            )
 
         current_data = data.TensorDataset(
             states_tensor, actions_tensor, ext_target, int_target, total_adv,
             next_states_tensor, log_prob_old
         )
-        return data.DataLoader(current_data, batch_size=BATCH_SIZE, num_workers=8)
+        return data.DataLoader(current_data, batch_size=BATCH_SIZE, num_workers=self.loader_num_workers)
 
     def train_step(self, dataloader):
         for i in range(EPOCH_STEPS):
             for data in dataloader:
-                data = [dt.to(self.device) for dt in data]
+                data = [dt.to(self.agent.device) for dt in data]
 
                 self.agent_optimizer.zero_grad()
                 loss = self.agent.get_loss(data)
@@ -247,61 +179,15 @@ class RNDTrainer:
         return {
             "Agent": self.agent.state_dict(),
             "Optimizer": self.agent_optimizer.state_dict(),
-            "Current_States": self.current_states,
-            "EnvObs_Stats": [
-                self.env_runner.observation_stats.mean,
-                self.env_runner.observation_stats.var
-            ],
             "N_Steps": self.n_steps,
             "N_Updates": self.n_updates,
-            "Log_Episodes": self.log_episode
         }
 
     def load_state_dict(self, state_dict):
         self.agent.load_state_dict(state_dict["Agent"])
         self.agent_optimizer.load_state_dict(state_dict["Optimizer"])
-        self.current_states = state_dict["Current_States"]
-        self.env_runner.observation_stats.mean, \
-            self.env_runner.observation_stats.var = state_dict["EnvObs_Stats"]
         self.n_steps = state_dict["N_Steps"]
         self.n_updates = state_dict["N_Updates"]
-        self.log_episode_offset = state_dict["Log_Episodes"]
-
-    def log_rollout_step_results(self, logger_hist):
-        for group in logger_hist:
-            log_reward, log_episode, log_steps = group
-            log_episode += self.log_episode_offset # Add offset
-
-            self.logger.add_scalar(
-                'data/reward_per_episode', log_reward, log_episode
-            )
-            self.logger.add_scalar(
-                'data/reward_per_updates', log_reward, self.n_updates
-            )
-            self.logger.add_scalar(
-                'data/num_steps', log_steps, log_episode
-            )
-        latest_log_episode = logger_hist[-1][1]
-        latest_log_episode += self.log_episode_offset
-
-        self.log_episode = latest_log_episode
-
-        self.logger.add_scalar(
-            'data/intrinsic_reward_per_episode', np.sum(
-                self.stored_data["intrinsic_reward"] / self.num_workers
-            ), self.log_episode
-        )
-        self.logger.add_scalar(
-            "data/intrinsic_reward_per_rollout", np.sum(
-                self.stored_data["intrinsic_reward"] / self.num_workers
-            ), self.n_updates
-        )
-        self.logger.add_scalar(
-            "data/max_probability_per_episode",
-            torch.softmax(
-                torch.from_numpy(self.stored_data["policy"]), -1
-            ).max(1)[0].mean().item(), self.log_episode
-        )
 
     def __del__(self):
-        self.rollout_runner.join(timeout=1)
+        self.conn_to_actor.join(timeout=1)
